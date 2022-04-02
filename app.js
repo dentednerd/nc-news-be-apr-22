414665b0a30c854c64b38f4e60b918e7b55c0b57
Joey Imlay <jo.imlay@gmail.com>
2022-04-02 14:08:14 +0100

    #12 adds custom error handling

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const { getTopics } = require('./controllers/topics.controllers');
-const { handle404s } = require('./errorHandling');
+const { getArticleById } = require('./controllers/articles.controllers');
+const { handle404s, handleCustomErrors } = require('./errorHandling');
 
 const app = express();
 app.use(express.json());
 
 app.get('/api/topics', getTopics);
+app.get('/api/articles/:article_id', getArticleById);
 
 app.all('/*', handle404s);
+app.use(handleCustomErrors);
 
 module.exports = app;
--- errorHandling.js
+++ errorHandling.js
@@ -1,3 +1,9 @@
-exports.handle404s = (req, res, next) => {
+exports.handle404s = (req, res) => {
   res.status(404).send({ msg: 'Not found' });
+};
+
+exports.handleCustomErrors = (err, req, res, next) => {
+  if (err.status) {
+    res.status(err.status).send({ msg: err.msg });
+  } else next(err);
 };
